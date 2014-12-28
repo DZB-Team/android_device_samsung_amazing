1450ca5bf385bc856263df31a30ec7c0d7a6a8a6
Sergey Karbainov <karbainovs@mail.ru>
2014-12-28 11:12:12 +0700

    Update device_amazing.mk

--- device_amazing.mk
+++ device_amazing.mk
@@ -14,8 +14,12 @@
 
 # Init files
 PRODUCT_COPY_FILES += \
+		device/samsung/amazing/ramdisk/fstab.gt-s6802board:root/fstab.gt-s6802board \
+		device/samsung/amazing/ramdisk/ueventd.gt-s6802board.rc:root/ueventd.gt-s6802board.rc \
    	device/samsung/amazing/ramdisk/init.gt-s6802board.sensors.rc:root/init.gt-s6802board.sensors.rc \
    	device/samsung/amazing/ramdisk/init.gt-s6802board.gps.rc:root/init.gt-s6802board.gps.rc
+   	device/samsung/amazing/ramdisk/init.gt-s6802board.fs.rc:root/init.gt-s6802board.fs.rc
+      device/samsung/amazing/ramdisk/init.bcm21553.rc:root/init.bcm21553.rc
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
